90da6442e5efdd79988367aefa1f3840331b972b
sujit <sujit@sujit-kumar>
2024-08-25 14:59:46 +0530

    adding to repo sts

---
+++ gitStsPractice/src/main/java/com/example/multiThreading/GitStsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.multiThreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitStsPracticeApplication.class, args);
+	}
+
+}
---
+++ gitStsPractice/src/test/java/com/example/multiThreading/GitStsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multiThreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
