0d1beb7e2f19e1d52f820bac8fa55301afb6f574
sujit-01 <sujittech06@gmail.com>
2024-08-29 00:59:34 +0530

    "modified Animal.java"

--- gitStsPractice/src/main/java/com/example/sujit/Animal.java
+++ gitStsPractice/src/main/java/com/example/sujit/Animal.java
@@ -1,8 +1,10 @@
 package com.example.sujit;
 
 public class Animal {
-	public static void main(String[] args) {	
-	System.out.println("hello dev");
+	public static void main(String[] args)
+	{
+	System.out.println("hello dev123");
 	}
+
 
 }
